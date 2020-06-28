18ef02c4fff8f3bb5a89ad02c738b3fd4101293e
Adrien-Schussoft <adrien.dessartre@gmail.com>
2020-06-28 23:18:51 +0200

    GUI major update

--- src/main/java/org/adrien/controller/PrimaryController.java
+++ src/main/java/org/adrien/controller/PrimaryController.java
@@ -2,7 +2,6 @@ package org.adrien.controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -16,6 +15,7 @@ import java.lang.reflect.Method;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import static org.adrien.controller.RegExpMatching.isValidInput;
@@ -53,6 +53,12 @@ public class PrimaryController implements Initializable {
     @FXML
     private BorderPane borderPane;
 
+    Alert alert = new Alert(Alert.AlertType.NONE);
+
+    Boolean save = false;
+    Boolean update = false;
+    Boolean delete = false;
+
     ObservableList<IClient> model = FXCollections.observableArrayList();
     IDao clientIDao = BeanFactory.getClientDao();
     IClient client = BeanFactory.getClient();
@@ -86,16 +92,22 @@ public class PrimaryController implements Initializable {
 
     /**
      * Save event to save data.
-     * @param event
+     * @param
      * @throws ClassNotFoundException,NoSuchMethodException,IllegalAccessException,InvocationTargetException
      */
     @FXML
-    private void save(ActionEvent event) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException, ClassNotFoundException, SQLException, InstantiationException {
+    private void save() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException, ClassNotFoundException, SQLException, InstantiationException {
         if(!vbox_form.isVisible()){
             clearInputs();
             setVboxVisible();
+            save = true;
+            setAction();
         }else{
-            if(isValidInput(text_nom.getText()) && isValidInput(text_prenom.getText()) && isValidInput(text_ville.getText())){
+            alert.setAlertType(Alert.AlertType.INFORMATION);
+            alert.setTitle("Save confirmation");
+            alert.setHeaderText(null);
+            alert.setContentText("Client "+text_nom.getText()+" "+text_prenom.getText()+" saved !");
+            if(vbox_form.isVisible() && save == true && isValidInput(text_nom.getText()) && isValidInput(text_prenom.getText()) && isValidInput(text_ville.getText())){
                 label_error.setVisible(false);
                 IClient client = BeanFactory.getClient();
                 client.setNom(text_nom.getText());
@@ -107,6 +119,8 @@ public class PrimaryController implements Initializable {
                 model.clear();
                 clientArrayList = (ArrayList<IClient>) methodList.invoke(clientIDao);
                 model.addAll(clientArrayList);
+                alert.showAndWait();
+                annuler();
             }
             else{
                 label_error.setText("Saisie incorrecte");
@@ -118,26 +132,34 @@ public class PrimaryController implements Initializable {
 
     /**
      * update event to update data.
-     * @param event
+     * @param
      * @throws ClassNotFoundException,NoSuchMethodException,IllegalAccessException,InvocationTargetException
      */
     @FXML
-    private void update(ActionEvent event) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, ClassNotFoundException {
+    private void update() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, ClassNotFoundException {
         if(!vbox_form.isVisible()){
             setVboxVisible();
+            update = true;
+            setAction();
         }else {
-            if(isValidInput(text_nom.getText()) && isValidInput(text_prenom.getText()) && isValidInput(text_ville.getText())) {
+            alert.setAlertType(Alert.AlertType.INFORMATION);
+            alert.setTitle("Update confirmation");
+            alert.setHeaderText(null);
+            alert.setContentText("Client updated !");
+            if (isValidInput(text_nom.getText()) && isValidInput(text_prenom.getText()) && isValidInput(text_ville.getText())) {
                 label_error.setVisible(false);
                 client.setId(lst_clients.getSelectionModel().getSelectedItem().getId());
                 client.setNom(text_nom.getText());
                 client.setPrenom(text_prenom.getText());
                 client.setVille(text_ville.getText());
                 Method methodUpdate = BeanFactory.getUpdate();
-                client = (IClient) methodUpdate.invoke(clientIDao,client);
+                client = (IClient) methodUpdate.invoke(clientIDao, client);
                 model.clear();
                 clientArrayList = (ArrayList<IClient>) methodList.invoke(clientIDao);
                 model.addAll(clientArrayList);
-            }else{
+                alert.showAndWait();
+                annuler();
+            } else {
                 label_error.setText("Saisie incorrecte");
                 label_error.setVisible(true);
                 System.out.println("Saisie invalide");
@@ -147,33 +169,55 @@ public class PrimaryController implements Initializable {
 
     /**
      * delete event to delete data.
-     * @param event
+     * @param
      * @throws ClassNotFoundException,NoSuchMethodException,IllegalAccessException,InvocationTargetException
      */
     @FXML
-    private void delete(ActionEvent event) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, ClassNotFoundException {
+    private void delete() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, ClassNotFoundException {
         if(!vbox_form.isVisible()){
             setVboxVisible();
+            delete = true;
+            setAction();
         }else {
-            client.setId(lst_clients.getSelectionModel().getSelectedItem().getId());
-            System.out.println(client.getId());
-            Method methodDelete = BeanFactory.getDelete();
-            client = (IClient) methodDelete.invoke(clientIDao,client);
-            model.clear();
-            clientArrayList = (ArrayList<IClient>) methodList.invoke(clientIDao);
-            model.addAll(clientArrayList);
-            clearInputs();
+            alert.setAlertType(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Delete confirmation");
+            alert.setHeaderText(lst_clients.getSelectionModel().getSelectedItem().getNom()+" "+lst_clients.getSelectionModel().getSelectedItem().getPrenom()+" will be deleted.");
+            alert.setContentText(null);
+            Optional<ButtonType> result = alert.showAndWait();
+            alert.setAlertType(Alert.AlertType.INFORMATION);
+            alert.setTitle("Delete confirmation");
+            alert.setHeaderText(null);
+            alert.setContentText("Client "+lst_clients.getSelectionModel().getSelectedItem().getNom()+" "+lst_clients.getSelectionModel().getSelectedItem().getPrenom()+" deleted !");
+            if (result.get() == ButtonType.OK) {
+                client.setId(lst_clients.getSelectionModel().getSelectedItem().getId());
+                System.out.println(client.getId());
+                Method methodDelete = BeanFactory.getDelete();
+                client = (IClient) methodDelete.invoke(clientIDao, client);
+                model.clear();
+                clientArrayList = (ArrayList<IClient>) methodList.invoke(clientIDao);
+                model.addAll(clientArrayList);
+                clearInputs();
+                alert.showAndWait();
+                annuler();
+            }
         }
     }
 
     /**
      * Cancel the user's input.
-     * @param event
+     * @param
      */
     @FXML
-    private void annuler(ActionEvent event) {
-    clearInputs();
-    setVboxUnvisible();
+    private void annuler() {
+        save = false;
+        update = false;
+        delete = false;
+        setAction();
+        clearInputs();
+        setVboxUnvisible();
+        btn_sauver.setDisable(false);
+        btn_update.setDisable(false);
+        btn_supprimer.setDisable(false);
     }
 
     private void clearInputs(){
@@ -188,6 +232,7 @@ public class PrimaryController implements Initializable {
     @FXML
     private void getDataModelSelected(){
          lst_clients.setOnMouseClicked(event1 -> {
+
              try {
                  Method methodFindById = BeanFactory.getFindById();
                  client = (IClient) methodFindById.invoke(clientIDao,lst_clients.getSelectionModel().getSelectedItem().getId());
@@ -197,6 +242,76 @@ public class PrimaryController implements Initializable {
              text_nom.setText(client.getNom());
              text_prenom.setText(client.getPrenom());
              text_ville.setText(client.getVille());
+        });
+    }
+
+    @FXML
+    private void setAction(){
+
+        System.out.println("Bool save = "+save.booleanValue());
+        System.out.println("Bool update = "+update.booleanValue());
+        System.out.println("Bool delete = "+delete.booleanValue());
+
+        if(save == true){
+            System.out.println("save");
+            btn_sauver.setStyle("-fx-background-color: forestgreen");
+            allSetDisable();
+        }
+        else if(update == true){
+            System.out.println("update");
+            btn_update.setStyle("-fx-background-color: forestgreen");
+            allSetDisable();
+        }
+        else if(delete == true){
+            System.out.println("delete");
+            btn_supprimer.setStyle("-fx-background-color: forestgreen");
+            allSetDisable();
+        }
+       if(save == false){
+            btn_sauver.setStyle("-fx-background-color: #E66A6A");
+        }
+        if(update == false){
+            btn_update.setStyle("-fx-background-color: #E66A6A");
+        }
+        if(delete == false){
+            btn_supprimer.setStyle("-fx-background-color: #E66A6A");
+        }
+
+    }
+
+    private void allSetDisable(){
+        btn_supprimer.setDisable(true);
+        btn_supprimer.setOpacity(1);
+        btn_sauver.setDisable(true);
+        btn_sauver.setOpacity(1);
+        btn_update.setDisable(true);
+        btn_update.setOpacity(1);
+    }
+
+    @FXML
+    private void action(){
+        btn_ok.setOnMouseClicked (event1 -> {
+            if(save == true){
+                try {
+                    save();
+                } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException | SQLException | InstantiationException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(update == true){
+                try {
+                    update();
+                } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(delete == true){
+                try {
+                    delete();
+                } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
         });
     }
 
